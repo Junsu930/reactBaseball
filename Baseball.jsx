fff7a361b20a8a40aaadaad67f4659b874e5b2b8
Junsus <119837015+Junsu930@users.noreply.github.com>
2023-07-18 08:48:39 +0900

    dd

--- Baseball.jsx
+++ Baseball.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Try from './Try';
 
 // 숫자 4개를 겹치지 않고 뽑기
 function getNumbers(){
@@ -22,6 +23,14 @@ class Baseball extends Component {
     };
     input;
 
+    fruits = [
+        {fruit:'사과',characteristic:'달다'},
+        {fruit:'딸기',characteristic:'상큼하다'},
+        {fruit:'배',characteristic:'아삭하다'},
+        {fruit:'복숭아',characteristic:'달콤새콤'},
+        {fruit:'포도',characteristic:'재밌다'},
+    ];
+
     render(){
         return(
             <>
@@ -31,15 +40,9 @@ class Baseball extends Component {
                 </form>
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
-                    {[
-                        {fruit:'사과',characteristic:'달다'},
-                        {fruit:'딸기',characteristic:'상큼하다'},
-                        {fruit:'배',characteristic:'아삭하다'},
-                        {fruit:'복숭아',characteristic:'달콤새콤'},
-                        {fruit:'포도',characteristic:'재밌다'},
-                    ].map((v)=>{
+                    {this.fruits.map((v, i)=>{
                         return (
-                            <li key={v.fruit + v.characteristic}><b>{v.fruit}</b> - {v.characteristic}</li>
+                           <Try value={v} index={i}/>
                         );
                     })}
                 </ul>
